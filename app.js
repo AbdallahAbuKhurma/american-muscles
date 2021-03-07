840d4ea0dbfb55e64d5c0ec5f68d8ac0a2abc787
Abdallah Abu Khurma <abdallahabukhurma@gmail.com>
2021-03-07 13:54:45 +0200

    web page added

---
+++ app.js
@@ -0,0 +1,25 @@
+var order = prompt ("which car do you prefer charger or corvette?")
+
+while(order != 'charger' && order != 'corvette')
+{
+    order = prompt ("which car do you prefer?")
+}
+
+var itemorder = '';
+if(order == 'charger')
+{
+    itemorder = 'img src="images/car1.png" />';
+}
+else
+{
+    itemorder = 'img src="images/car2.png" />';
+}
+
+var numofcars = prompt("How many car would you like to see?")
+var result = '';
+for(var i = 0;i < numofcars; i++)
+{
+    result = result + itemorder
+}
+
+document.write(result);
