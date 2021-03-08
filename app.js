24f9fe074cace2fe271bbf5450aa3d5158c7401d
Abdallah Abu Khurma <abdallahabukhurma@gmail.com>
2021-03-08 14:33:28 +0200

    app.js edited

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var order = prompt ("which car do you prefer charger or corvette?")
 
-while(order != 'charger' && order != 'corvette')
+var choice = function() {
+    while(order != 'charger' && order != 'corvette')
 {
     order = prompt  ("which car do you prefer charger or corvette?")
 
 }
+}
+
+choice();
 
 var itemorder = '';
 if(order == 'charger')
@@ -23,4 +27,7 @@ for(var i = 0;i < numofcars; i++)
     result = result + itemorder
 }
 
+
 document.write(result);
+
+
